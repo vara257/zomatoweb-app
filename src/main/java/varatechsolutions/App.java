4be83c0a7782aaa3a267caa3eb3f97d00494c21c
vara257 <t.varaprasad257@gmail.com>
2022-11-04 10:10:47 +0530

    initial commit

---
+++ src/main/java/varatechsolutions/App.java
@@ -0,0 +1,13 @@
+package varatechsolutions;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
